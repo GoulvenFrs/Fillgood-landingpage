df8723605513e6b7880214c7a11adfaf0aa6c021
Goulven <goulven.friess@gmail.com>
2023-05-16 08:53:42 -0400

    Update Roadmap.jsx

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -13,22 +13,22 @@ export default function Roadmap() {
                 <div className="line"></div>
                 <div className="bloc" data-aos="zoom-in">
                   <div className="number">1</div>
-                  <h4>Launch Roadmap</h4>
+                  <h4>Q1/Q2 2023 : Landing page & Discord launch</h4>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum a repellendus nemo minus alias officia culpa ab. </p>
                 </div>
                 <div className="bloc" data-aos="zoom-in">
                   <div className="number off">2</div>
-                  <h4>Launch Presale</h4>
+                  <h4>Q2 2023 : Testnet launch & Litepaper</h4>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum a repellendus nemo minus alias officia culpa ab. </p>
                 </div>
                 <div className="bloc" data-aos="zoom-in">
                   <div className="number off">3</div>
-                  <h4>Mint the remaining NFTs on OpenSea</h4>
+                  <h4>Q3 2023 : Whitepaper & Community</h4>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum a repellendus nemo minus alias officia culpa ab. </p>
                 </div>
                 <div className="bloc" data-aos="zoom-in">
                   <div className="number off">4</div>
-                  <h4>Mint the remaining NFTs on OpenSea</h4>
+                  <h4>Q4 2023 : Mainnet launch & Airdrop</h4>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum a repellendus nemo minus alias officia culpa ab. </p>
                 </div>
               </div>
