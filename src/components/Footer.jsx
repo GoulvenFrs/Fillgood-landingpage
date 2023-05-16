c57f86af98cb43ab0b68b777ec99ff20a5d6a232
Goulven <goulven.friess@gmail.com>
2023-05-16 09:50:06 -0400

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,25 +2,27 @@
 import Image from 'next/image'
 import Link from 'next/link';
 
-import Logo from "../images/logo2.png"
+import Logo from "../images/logo1.png"
+import Linkedin from "../images/socials/linkedin.png"
+import Discord from "../images/socials/discord.png"
+import Twitter from "../images/socials/twitter.png"
 
 export default function Footer() {
     return (
       <div className="Footer">
-        <Link href="#home" className="MenuLogoContainer">
+        <Link href="#home" className="FooterLogoContainer">
           <div className="MenuImg">
               <Image className="LogoImg" src={Logo} alt=""/>
           </div>
           <p className="LogoTitle">Fillgood</p>
         </Link>
 
-        <div className="MenuItemContainer">
-            <Link href="#sport" className="MenuItem">Accueil</Link>
-            <Link href="#sport" className="MenuItem">Sport</Link>
-            <Link href="#sport" className="MenuItem">Agenda</Link>
-        </div>
-        <div className="ConnectButton"> 
-            {/* <ConnectButton /> */}
+        <div className="FooterItemContainer">
+          <p className="FooterText" >FOLLOW US </p>
+          <Image className="FooterSocials" src={Discord} alt="Discord"/>
+          <Image className="FooterSocials" src={Linkedin} alt="Linkedin"/>
+          <Image className="FooterSocials" src={Twitter} alt="Twitter"/>
+
         </div>
 
       </div>
