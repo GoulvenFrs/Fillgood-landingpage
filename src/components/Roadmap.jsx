f0bb9c2b8676ff0e58ffd770c33a9df92fbf7249
Goulven <goulven.friess@gmail.com>
2023-05-16 09:09:43 -0400

    Update Roadmap.jsx

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -14,22 +14,22 @@ export default function Roadmap() {
                 <div className="bloc" data-aos="zoom-in">
                   <div className="number">1</div>
                   <h4>Q1/Q2 2023 : Landing page & Discord launch</h4>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum a repellendus nemo minus alias officia culpa ab. </p>
+                  <p>Creation and launch of the landing page, the Discord and other social networks to start communicating about the project and building the community. </p>
                 </div>
                 <div className="bloc" data-aos="zoom-in">
                   <div className="number off">2</div>
                   <h4>Q2 2023 : Testnet launch & Litepaper</h4>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum a repellendus nemo minus alias officia culpa ab. </p>
+                  <p>Launch of the Dapp on testnet, our beta testers will test all the features as well as an audit firm. The litepaper will be released.  </p>
                 </div>
                 <div className="bloc" data-aos="zoom-in">
                   <div className="number off">3</div>
-                  <h4>Q3 2023 : Whitepaper & Community</h4>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum a repellendus nemo minus alias officia culpa ab. </p>
+                  <h4>Q3 2023 : Whitepaper & Community growth</h4>
+                  <p>The Whitepaper will be released and we will focus on the growth of the community on this quarter. </p>
                 </div>
                 <div className="bloc" data-aos="zoom-in">
                   <div className="number off">4</div>
                   <h4>Q4 2023 : Mainnet launch & Airdrop</h4>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum a repellendus nemo minus alias officia culpa ab. </p>
+                  <p>Launch of the Dapp on mainnet and airdrop for beta testers and the community.</p>
                 </div>
               </div>
 
