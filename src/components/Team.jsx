d4d5e5878cfa2d3022d0f85d5489b5f574fe9820
Goulven <goulven.friess@gmail.com>
2023-05-15 20:25:47 -0400

    Update Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -19,7 +19,7 @@ export default function Team() {
                   William / <span className="job">General Management</span>
                 </h4>
                 <p className="memberDescription">
-                William studied at the University of Corte and holds a master's degree in tourism economics. After ten years of experience in the entertainment industry, he turned his career towards corporate finance and then trained in blockchain technology at the Alyra school. Convinced of the importance of physical activity for the general well-being of the community, William is now helping to build the Fillgood project with the mission of helping the platform's users to practice sports more regularly.
+                William studied at the University of Corte and holds a master&apos;s degree in tourism economics. After ten years of experience in the entertainment industry, he turned his career towards corporate finance and then trained in blockchain technology at the Alyra school. Convinced of the importance of physical activity for the general well-being of the community, William is now helping to build the Fillgood project with the mission of helping the platform&apos;s users to practice sports more regularly.
                 </p>
               </div>
               <div className="member" data-aos="zoom-in">
@@ -28,7 +28,7 @@ export default function Team() {
                   Goulven / <span className="job">Technical management</span>
                 </h4>
                 <p className="memberDescription">
-                Goulven is the current CTO of the platform. He is a general engineer and then specialized in IT project management. His experience as a Product Manager gives him an understanding of the market, its products and marketing knowledge that is important to Fillgood's development. Goulven recently trained in the specifics of blockchain at the Alyra school and now masters the creation of DApps with Truffle, Hardhat, Ethers.js and Web3.js.
+                Goulven is the current CTO of the platform. He is a general engineer and then specialized in IT project management. His experience as a Product Manager gives him an understanding of the market, its products and marketing knowledge that is important to Fillgood&apos;s development. Goulven recently trained in the specifics of blockchain at the Alyra school and now masters the creation of DApps with Truffle, Hardhat, Ethers.js and Web3.js.
                 </p>
                 
               </div>
