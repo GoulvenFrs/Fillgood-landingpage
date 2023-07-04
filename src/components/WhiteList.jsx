e4cba8534237b08251881943b0a8b5ded584de43
Goulven <goulven.friess@gmail.com>
2023-07-04 14:36:47 -0400

    Update WhiteList.jsx

--- src/components/WhiteList.jsx
+++ src/components/WhiteList.jsx
@@ -6,7 +6,7 @@ import Patch from "../images/pastille2.png";
 export default function WhiteList() {
   // Call the "useForm" hook in your function component
   const { state, submit } = useForm({
-    id: "D1Or4ZyMxI",
+    id: "3Xwe8FEA3u",
   });
 
   // Success message
@@ -17,9 +17,9 @@ export default function WhiteList() {
     <>
     <p className="ProductSectionSubTitle2 top"   data-aos="zoom-in" data-aos-duration="300"/*  href="https://discord.gg/vbaEg8rv"  target="_blank" */ >Join the Waitlist :</p>
     <form  className="WhiteList" onSubmit={submit}>         
-        <input  data-aos="zoom-in" data-aos-duration="200" id="email" type="email" name="email" placeholder="Email" className="InputWhiteList" required/>
+        <input id="email" type="email" name="email" data-aos="zoom-in" data-aos-duration="200"  placeholder="Email" className="InputWhiteList" required/>
         <input id="message" name="message" className="InputWhiteListMessage" defaultValue="WhiteList"/>
-        <input type="hidden" name="_next" value="https://fillgood.io"></input>
+        {/* <input type="hidden" name="_next" value="https://fillgood.io"></input> */}
         
         <button /* className="ButtonWhiteList" */ type="submit">
           <Image className="Patch" src={Patch} alt=""/>
