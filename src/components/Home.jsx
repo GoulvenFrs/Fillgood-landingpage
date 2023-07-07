e32f51a44e299ca19a0d530f4b830d0726be9a2d
Goulven <goulven.friess@gmail.com>
2023-07-07 12:14:11 -0400

    wording

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,14 +15,15 @@ export default function Home() {
             <p data-aos="zoom-in" data-aos-duration="200"className="HomeSectionTitle1" >HAVE FUN</p>
             <p data-aos="zoom-in" data-aos-duration="300"className="HomeSectionTitle1" >AND COLLECT</p>
             <p data-aos="zoom-in" data-aos-duration="400"className="HomeSectionTitle1" >YOUR REWARDS !</p>
-            <p data-aos="zoom-in" data-aos-duration="500"className="HomeSectionSubTitle1 top" >Sport makes you Fillgood !</p>
+            <p data-aos="zoom-in" data-aos-duration="500"className="HomeSectionSubTitle1 top" >Fillgood is a physical activity stimulation solution using the Proof of Attendance Protocol to
+	reward its users.</p>
             {/* <p data-aos="zoom-in" data-aos-duration="600"className="HomeSectionSubTitle1" >and earn your first Figos !</p> */}
             <div className="HomeItemContainer">
               <p className="HomeSocialText" data-aos="zoom-in" >FOLLOW US </p>
               <div className="HomeSocialGroup" >
-                <a data-aos="zoom-in" data-aos-duration="1000" href="https://discord.gg/VAUWV3M9C6" target="_blank"><Image className="HomeSocials" src={Discord} alt="Discord"/></a>
-                <a data-aos="zoom-in" data-aos-duration="2000"href="https://www.linkedin.com/company/fillgood-fr" target="_blank"><Image className="HomeSocials" src={Linkedin} alt="Linkedin"/></a>
-                <a data-aos="zoom-in" data-aos-duration="3000"href="https://twitter.com/FILLGOODoff" target="_blank"><Image className="HomeSocials" src={Twitter} alt="Twitter"/></a>
+                <a data-aos="zoom-in" data-aos-duration="600" href="https://discord.gg/VAUWV3M9C6" target="_blank"><Image className="HomeSocials" src={Discord} alt="Discord"/></a>
+                <a data-aos="zoom-in" data-aos-duration="700"href="https://www.linkedin.com/company/fillgood-fr" target="_blank"><Image className="HomeSocials" src={Linkedin} alt="Linkedin"/></a>
+                <a data-aos="zoom-in" data-aos-duration="800"href="https://twitter.com/FILLGOODoff" target="_blank"><Image className="HomeSocials" src={Twitter} alt="Twitter"/></a>
               </div>
               
             </div>
